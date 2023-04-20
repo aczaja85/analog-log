23731a4dc3271a692a7d042fe4dd90038a60452c
aczaja85 <80494298+aczaja85@users.noreply.github.com>
2023-04-20 15:53:45 -0600

    cleaned up config

--- webpack.config.js
+++ webpack.config.js
@@ -9,28 +9,12 @@ module.exports = {
   },
   devServer: {
     proxy: {
-      // UPDATE THIS IF URL IS NOT /API. UPDATE IT TO WHATEVER THE ROUTE IS.
       '/api/**': {
-        // ANY REQUEST WILL BE SENT TO OUR NODE SERVER RUNNING ON PORT 3000 AND WILL BE PROCESSED FROM THERE.
-        // UPDATE PORT NUMBER IF NECESSARY
         target: 'http://localhost:3000',
         secure: false,
       },
     },
   },
-  // devServer: {
-  //   static: {
-  //     directory: path.join(__dirname, 'dist'),
-  //   },
-  //   historyApiFallback: true,
-  //   port: 8080,
-  //   hot: true,
-  //   compress: true,
-  //   proxy: {
-  //     '/**': 'http://localhost:3000',
-  //   },
-  //   watchFiles: ['src/**'],
-  // },
   plugins: [
     new HTMLWebpackPlugin({
       template: './index.html',
@@ -60,17 +44,3 @@ module.exports = {
     extensions: ['.js', '.jsx'],
   },
 };
-
-// devServer: {
-//   static: {
-//     directory: path.join(__dirname, 'build')
-//   },
-//   historyApiFallback: true,
-//   port: 8080,
-//   hot: true,
-//   compress: true,
-//   proxy: {
-//     '/**': 'http://localhost:3000'
-//   },
-//   watchFiles: ['client/**']
-// }
