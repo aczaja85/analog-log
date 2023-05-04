7e20cb1a9094bba04747a613779f6fd1d3604619
aczaja85 <80494298+aczaja85@users.noreply.github.com>
2023-05-04 14:45:59 -0600

    refactored auth on server side

--- server/controllers/albumsController.ts
+++ server/controllers/albumsController.ts
@@ -8,6 +8,10 @@ interface AlbumsController {
   deleteAlbum(req: Request, res: Response, next: NextFunction): void;
 }
 
+const CLIENT_ID = '38ef7ce6023b4c1e8b6057767a412e0c';
+const CLIENT_SECRET = 'd5ea58227a2e4ac281ddf010163f8d05';
+let token = '';
+
 const albumsController: AlbumsController = {
   //get all albums in album collection
   getAlbums: (req, res, next) => {
@@ -25,7 +29,36 @@ const albumsController: AlbumsController = {
   //add album to collection
   addAlbum: async (req, res, next) => {
     const newAlbum = req.body;
-    const { token, artist, album } = req.body;
+    const { artist, album } = req.body;
+
+    //if token doesn't exist fetch authtoken from api
+    if (token === '') {
+      const authParams = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body:
+          'grant_type=client_credentials&client_id=' +
+          CLIENT_ID +
+          '&client_secret=' +
+          CLIENT_SECRET,
+      };
+
+      try {
+        const res = await fetch(
+          'https://accounts.spotify.com/api/token',
+          authParams
+        );
+        const auth = await res.json();
+        token = auth.access_token;
+      } catch (error) {
+        return next({
+          log: 'albumController.addAlbum',
+          message: 'Error fetching token from Spotify API',
+        });
+      }
+    }
 
     //get album cover art from spotify api
     const searchParams = {
